74363e1849fa105e6facb7a9b94a60b6511083ac
Kevin Barton <kbarton@tamusa.edu>
2022-11-18 19:21:55 -0700

    Update config.php

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 
-define('DB_SERVER', 'localhost');
+define('DB_SERVER', 'sqlimysql');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'sql_injection');
+define('DB_PASSWORD', 'karbach');
+define('DB_NAME', 'fuzzydog');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
